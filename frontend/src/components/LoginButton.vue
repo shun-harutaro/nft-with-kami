01d99841ba6c20394a0b16fbacfa99e8bf8b27c7
shun-harutaro <shun_harutaro@protonmail.com>
2024-11-20 10:09:19 +0900

    とりあえずログインできた

---
+++ frontend/src/components/LoginButton.vue
@@ -0,0 +1,21 @@
+<template>
+  <button @click="handleLogin">LINEでログイン</button>
+</template>
+
+<script>
+import { useAuth } from "@/composables/useAuth";
+
+export default {
+  setup() {
+    const { login } = useAuth();
+
+    function handleLogin() {
+      login();
+    }
+
+    return {
+      handleLogin,
+    };
+  },
+};
+</script>
