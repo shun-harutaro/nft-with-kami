a2a39c99570889cec21a8452bfef0048cd88337c
shun-harutaro <shun_harutaro@protonmail.com>
2024-11-21 09:55:58 +0900

    gptのロジックを追加

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,17 @@
+from contextlib import asynccontextmanager
 from fastapi import FastAPI
+from utils.config import check_env_variables
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    check_env_variables()
+    yield
+    print("Shutting down...")
+
 
 app = FastAPI()
+
 
 @app.get("/")
 async def root():
---
+++ backend/utils/config.py
@@ -0,0 +1,26 @@
+import os
+
+
+def check_env_variables():
+    env_vars: list[str] = [
+        "OPENAI_API_KEY",
+        "OPENAI_ASSISTANT_ID",
+        "OPENAI_THREAD_ID"
+    ]
+    missing_vars = [var for var in env_vars if os.getenv(var) is None]
+    if missing_vars:
+        print(
+            f"WARN Missing environment variables: {', '.join(missing_vars)}"
+        )
+
+
+def get_openai_api_key() -> str | None:
+    return os.getenv("OPENAI_API_KEY")
+
+
+def get_openai_assistant_id() -> str | None:
+    return os.getenv("OPENAI_ASSISTANT_ID")
+
+
+def get_openai_thread_id() -> str | None:
+    return os.getenv("OPENAI_THREAD_ID")
