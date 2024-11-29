55213342bda34820e922383e2ec9453d9022d6c5
shun-harutaro <shun_harutaro@protonmail.com>
2024-11-29 11:00:01 +0900

    アクセストークン取得できた

--- backend/main.py
+++ backend/main.py
@@ -3,7 +3,7 @@ from contextlib import asynccontextmanager
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
-from routers import gpt, location, line, auth
+from routers import gpt, location, auth
 from utils.config import check_env_variables
 
 
@@ -17,7 +17,6 @@ async def lifespan(app: FastAPI):
 app = FastAPI(lifespan=lifespan)
 app.include_router(location.router)
 app.include_router(gpt.router)
-app.include_router(line.router)
 app.include_router(auth.router)
 
 
--- backend/routers/auth.py
+++ backend/routers/auth.py
@@ -4,6 +4,7 @@ from urllib.parse import urlencode
 
 from fastapi.responses import RedirectResponse
 from utils.config import get_line_client_id
+from services.line import get_token
 
 router = APIRouter()
 CLIENT_ID = get_line_client_id()
@@ -12,14 +13,22 @@ CLIENT_ID = get_line_client_id()
 @router.get("/auth/login")
 async def login():
     state = secrets.token_urlsafe(16)
-#    state_store[state] = True
+    # request.session["state"] = state
     base_url = "https://access.line.me/oauth2/v2.1/authorize"
     params = {
         "response_type": "code",
         "client_id": CLIENT_ID,
-        "redirect_uri": "http://localhost:3000/callback",
+        "redirect_uri": "http://localhost:8000/auth/callback",
         "state": state,
         "scope": "profile openid",
     }
     redirect_url = f"{base_url}?{urlencode(params)}"
     return RedirectResponse(url=redirect_url)
+
+
+@router.get("/auth/callback")
+async def auth_callback(code: str, state: str):
+    # TODO stateの検証
+    token_response = await get_token(code)
+
+    return {"state": state, "res": token_response}
