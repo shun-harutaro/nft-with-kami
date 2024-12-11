e33bf6b97b7886aa817fdca9e160edd5cafc9a1f
iamkawaryo <ryosukekawahara0725@gmail.com>
2024-12-11 17:58:40 +0900

    おみくじのvueファイルをいったん作りました (#59)
    Co-authored-by: NOZAKI Shuntaro <60352276+shun-harutaro@users.noreply.github.com>
    Co-authored-by: shun-harutaro <shun_harutaro@protonmail.com>

--- frontend/src/router.js
+++ frontend/src/router.js
@@ -2,10 +2,12 @@ import { createRouter, createWebHistory } from "vue-router"
 
 import Login from "./views/Login.vue"
 import Location from "./views/Location.vue"
+import Omikuji from "./views/Omikuji.vue"
 
 const routes = [
   { path: "/", component: Login },
   { path: "/location", component: Location },
+  { path: "/omikuji", component: Omikuji },
 ]
 
 const router = createRouter({
