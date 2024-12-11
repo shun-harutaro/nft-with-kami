e60e524c56862be2505917ac67a457a9e8f1706a
iamkawaryo <ryosukekawahara0725@gmail.com>
2024-12-11 18:11:15 +0900

    神社選択後の神降臨画面を作成し、神社選択からの画面遷移を設定しました。

--- frontend/src/router.js
+++ frontend/src/router.js
@@ -2,10 +2,12 @@ import { createRouter, createWebHistory } from "vue-router"
 
 import Login from "./views/Login.vue"
 import Location from "./views/Location.vue"
+import Godcome from "./views/Godcome.vue"
 
 const routes = [
   { path: "/", component: Login },
   { path: "/location", component: Location },
+  { path: "/godcome", component: Godcome },
 ]
 
 const router = createRouter({
