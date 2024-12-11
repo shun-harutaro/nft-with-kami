0dd6c7148f4552332c7aa8a7e96314b615f567f0
root <root@LAPTOP-6H0Q0K90.>
2024-12-11 10:49:41 +0900

    始めよう画面実装

--- frontend/src/components/LoginButton.vue
+++ frontend/src/components/LoginButton.vue
@@ -1,5 +1,12 @@
 <template>
-  <button @click="handleLogin">LINEでログイン</button>
+  <button @click="handleLogin">
+    <img
+        loading="lazy"
+        src="https://cdn.builder.io/api/v1/image/assets/TEMP/4a8febd21bfe1feaeaa099720a3e2ffe159c07ff180a36ffd7064a04500b571c?placeholderIfAbsent=true&apiKey=b0ba032ba04c496389e8dea963537aea"
+        class="gallery-footer"
+        alt="Gallery Footer"
+        />
+  </button>
 </template>
 
 <script>
--- frontend/src/views/home.vue
+++ frontend/src/views/home.vue
@@ -1,37 +1,42 @@
+<script setup>
+  import LoginButton from "@/components/LoginButton.vue"
+</script>
+
 <template>
   <div class="gallery-container">
     <div class="gallery-wrapper">
+      <!--神社-->
       <img
         loading="lazy"
         src="https://cdn.builder.io/api/v1/image/assets/TEMP/03f63f5fe13b4cb994f7f82af912ed64800f4fba07839ebe49c033b475648fea?placeholderIfAbsent=true&apiKey=b0ba032ba04c496389e8dea963537aea"
         class="gallery-background"
         alt=""
       />
+      <!--NFTwith神-->
       <img
         loading="lazy"
         src="https://cdn.builder.io/api/v1/image/assets/TEMP/746a97cb32fcc0e663949dd8a4e00662dd6fb2957f4ffb8fcbbd6411133003e9?placeholderIfAbsent=true&apiKey=b0ba032ba04c496389e8dea963537aea"
         class="gallery-header"
         alt="Gallery Header"
       />
+      <!--太陽モワモワ-->
       <img
         loading="lazy"
         src="https://cdn.builder.io/api/v1/image/assets/TEMP/d58f3000989a01d360aa6b718bf2a1da619b65250a41c5c91f8d6192282d4246?placeholderIfAbsent=true&apiKey=b0ba032ba04c496389e8dea963537aea"
         class="gallery-content"
         alt="Gallery Content"
       />
-      <img
-        loading="lazy"
-        src="https://cdn.builder.io/api/v1/image/assets/TEMP/4a8febd21bfe1feaeaa099720a3e2ffe159c07ff180a36ffd7064a04500b571c?placeholderIfAbsent=true&apiKey=b0ba032ba04c496389e8dea963537aea"
-        class="gallery-footer"
-        alt="Gallery Footer"
-      />
+      <!--LINEログイン-->
+      <LoginButton style="position: relative;z-index: 10;">
+      </LoginButton>
+
     </div>
   </div>
 </template>
 
 <style scoped>
 .gallery-container {
-  background-color: blue;
+  background: linear-gradient(to bottom, #35ECBB, #FF9C12);
   display: flex;
   max-width: 480px;
   width: 100%;
@@ -72,6 +77,8 @@
   object-position: center;
   width: 100%;
   margin-top: 89px;
+  position: relative;
+  top: 20px;
 }
 
 .gallery-footer {
@@ -81,6 +88,9 @@
   width: 207px;
   align-self: center;
   max-width: 100%;
+  position: relative;
+  top: 50px;
+  z-index: 100000;
 }
 
 .visually-hidden {
