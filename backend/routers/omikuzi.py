408152bb62a4b5c4e9f2f32bf85314bb78f90e72
imtkgtr <imtkgtr@gmail.com>
2024-12-11 12:02:01 +0900

    おみくじ生成のために色々生成を加えました。

--- backend/routers/omikuzi.py
+++ backend/routers/omikuzi.py
@@ -1,7 +1,10 @@
-from fastapi import APIRouter, BackgroundTasks
+from fastapi import APIRouter, BackgroundTasks,Query
+from pydantic import HttpUrl
 from fastapi.responses import FileResponse
+from fastapi import UploadFile
 from schemas.omikuzi_schema import OmikuziText
 from services.omikuzi_service import generate_omikuzi,remove_file
+from PIL import Image
 
 router = APIRouter()
 omikuzi_template = '/app/services/omikuzi/omikuzi_template.png'
@@ -15,9 +18,13 @@ font = '/app/services/omikuzi/玉ねぎ楷書激無料版v7改.ttf'
     summary="テキストからおみくじの生成",
     response_model=None,
 )
-async def gpt(omikuzi_text: OmikuziText, background_tasks: BackgroundTasks):
+async def omikuzi(omikuzi_text: OmikuziText, 
+                  background_tasks:BackgroundTasks,
+                  shrine_name:str=Query(default='拳母神社'), 
+                  icon_url:HttpUrl=Query(...)
+    ):
     # おみくじ画像を生成
-    generated_image_path = generate_omikuzi(img_path=omikuzi_template, font_path=font, omikuzi_text=omikuzi_text)
+    generated_image_path = generate_omikuzi(img_path=omikuzi_template, font_path=font, omikuzi_text=omikuzi_text, shrine_name=shrine_name, icon_url=icon_url)
 
     # 送信後に一時ファイルを削除
     background_tasks.add_task(remove_file, generated_image_path)
--- backend/services/omikuzi_service.py
+++ backend/services/omikuzi_service.py
@@ -1,28 +1,23 @@
 from PIL import Image,ImageDraw, ImageFont
 from tempfile import NamedTemporaryFile
 import os
+from datetime import datetime
+import requests
+import io
+
 
 #縦向きの文字を追加
 def add_text_to_image_vertical(img, text, position, font_path, font_size, max_length=0, text_color=(0, 0, 0)):
     draw = ImageDraw.Draw(img)
     font = ImageFont.truetype(font_path, font_size)
     x, y = position
-    # 指定文字数で改行
-    if max_length > 0:
+    if(max_length != 0):
         lines = wrap_text(text, max_length)
         for line in lines:
-            for char in line:  # 1文字ずつ描画
-                if(char == '。' or  char == '、'):
-                    draw.text((x+35, y-35), char, font=font, fill=text_color)
-                else:
-                    draw.text((x, y), char, font=font, fill=text_color)
-                y += font_size  # 文字ごとに下へ移動
+            draw.text((x, y), line, fill=text_color, font=font,direction='ttb')
             x -= font_size * 1.2  # 行間を調整
-            y = position[1]  # 縦位置をリセット
     else:
-        for char in text:
-            draw.text((x, y), char, font=font, fill=text_color)
-            y += font_size  # 文字ごとに下へ移動
+        draw.text(position, text, fill=text_color, font=font,direction='ttb')
     return img
 
 #横向きの文字を追加
@@ -41,8 +36,9 @@ def add_text_to_image_horizontal(img, text, position, font_path, font_size, max_
 
 
 #画像を追加
-def add_image_to_image(base_img,add_img ,position, size):
-    base_img.paste(add_img.resize(size), position)
+def add_image_to_image(base_img, add_img, position, size):
+    resized_img = add_img.resize(size, Image.Resampling.LANCZOS)
+    base_img.paste(resized_img, position, mask=resized_img)
     return base_img
 
 #指定文字数で改行を行う
@@ -55,21 +51,50 @@ def remove_file(file_path: str):
         os.remove(file_path)
 
 #おみくじの生成
-def generate_omikuzi(img_path, font_path, omikuzi_text):
+def generate_omikuzi(img_path, font_path, omikuzi_text, shrine_name, icon_url):
     img = Image.open(img_path).copy()
     input_text = omikuzi_text.dict()
-
-    img = add_text_to_image_vertical(img, input_text["運勢"], (1600, 100), font_path, 250)
+    img = add_text_to_image_vertical(img, shrine_name, (1830, 50), font_path, 50)
+    img = add_text_to_image_vertical(img, input_text["運勢"], (1630, 100), font_path, 200)
     img = add_text_to_image_vertical(img, input_text["願望"], (1430, 50), font_path, 50, 20)
     img = add_text_to_image_vertical(img, input_text["健康"], (1210, 50), font_path, 50, 20)
     img = add_text_to_image_vertical(img, input_text["金運"], (990, 50), font_path, 50, 20)
     img = add_text_to_image_vertical(img, input_text["学問"], (770, 50), font_path, 50, 20)
     img = add_text_to_image_vertical(img, input_text["恋愛"], (550, 50), font_path, 50, 20)
-    img = add_text_to_image_vertical(img, input_text["ひとこと"], (340, 50), font_path, 50, 20)
+    img = add_text_to_image_vertical(img, input_text["神託"], (320, 50), font_path, 50, 20)
+    icon = load_image_from_url(icon_url)
 
-    # 一時ファイルに保存
+    width, height = icon.size
+    rounded_icon = create_rounded_icon(icon, (width,height))
+    
+    img = add_image_to_image(img, rounded_icon, (1610,530), (245,245))
     temp_file = NamedTemporaryFile(delete=False, suffix=".png")
     img.save(temp_file.name)
     temp_file.close()
 
     return temp_file.name
+
+def load_image_from_url(url: str) -> Image.Image:
+    try:
+        response = requests.get(url, stream=True)
+        response.raise_for_status() 
+        image = Image.open(io.BytesIO(response.content))
+        return image
+    except requests.exceptions.RequestException as e:
+        print(f"Failed to fetch image from URL: {e}")
+        return None
+    except Exception as e:
+        print(f"Failed to process image: {e}")
+        return None
+    
+
+
+def create_rounded_icon(icon, size):
+    width, height = size
+    icon = icon.resize((width, height), Image.Resampling.LANCZOS).convert("RGBA")
+    mask = Image.new("L", (width, height), 0)  
+    draw = ImageDraw.Draw(mask)
+    draw.ellipse((0, 0, width, height), fill=255)  
+    rounded_icon = Image.new("RGBA", (width, height), (0, 0, 0, 0))  
+    rounded_icon.paste(icon, (0, 0), mask=mask)  
+    return rounded_icon
