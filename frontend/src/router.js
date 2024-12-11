349eb30cbf2731d52cd323e8fbc69287450e974e
iamkawaryo <ryosukekawahara0725@gmail.com>
2024-12-11 16:38:28 +0900

    ログイン画面のレイアウトを作成しました

--- frontend/src/router.js
+++ frontend/src/router.js
@@ -2,8 +2,6 @@ import { createRouter, createWebHistory } from "vue-router"
 
 import Login from "./views/Login.vue"
 import Location from "./views/Location.vue"
-import User from "./views/User.vue"
-
 const routes = [
   { path: "/", component: Login },
   { path: "/location", component: Location },
