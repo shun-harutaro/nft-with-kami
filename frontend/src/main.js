8f65e099e726760b0c3cf522b4123bf34898fb3f
shun-harutaro <shun_harutaro@protonmail.com>
2024-12-12 11:33:33 +0900

    wip

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -7,7 +7,7 @@ import router from "./router"
 import axios from 'axios';
 
 axios.defaults.withCredentials = true;
-//axios.defaults.baseURL = 'https://api.example.com';
+axios.defaults.baseURL = import.meta.env.VITE_API_URL;
 
 const pinia  = createPinia()
 createApp(App)
