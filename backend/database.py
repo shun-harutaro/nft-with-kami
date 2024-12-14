e424ad6af40afad5eb06cbfb8090762f556ab497
NOZAKI Shuntaro <60352276+shun-harutaro@users.noreply.github.com>
2024-12-14 09:08:14 +0900

    データベースへの接続 (#136)

--- backend/database.py
+++ backend/database.py
@@ -1,12 +1,13 @@
-# import ssl
-from typing import AsyncGenerator  # , Dict
+import ssl
+from typing import AsyncGenerator, Dict
 
 from sqlalchemy.ext.asyncio import create_async_engine
 from sqlmodel import SQLModel
 from sqlmodel.ext.asyncio.session import AsyncSession
 
-# from config import get_db_cert_path, get_db_object
+from utils.config import get_db_cert_path, get_db_object, get_is_dev_mode
 
+IS_DEV_MODE: bool = get_is_dev_mode()
 
 def create_dev_async_engine():
     from sqlalchemy.engine.url import URL
@@ -22,7 +23,6 @@ def create_dev_async_engine():
     return async_engine
 
 
-"""
 def create_prod_async_engine():
     from sqlalchemy.engine.url import URL
 
@@ -37,17 +37,16 @@ def create_prod_async_engine():
         database=DB_OBJECT["database"],
         query={"charset": "utf8"},
     )
-    async_engine = create_engine(
-        url, connect_args={"ssl": ssl_context}, echo=True, future=True
+    async_engine = create_async_engine(
+        url, connect_args={"ssl": ssl_context}, echo=True
     )
     return async_engine
-"""
 
 
 # 環境に応じたエンジンを作成
 async_engine = (
     create_dev_async_engine()
-)  # if IS_DEV_MODE else create_prod_async_engine()
+) if IS_DEV_MODE else create_prod_async_engine()
 
 # 非同期セッションの作成
 async_session = AsyncSession(async_engine)
